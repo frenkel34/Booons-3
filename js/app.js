fe3566f874f89a014df88ddaf4fa5f3ac8e77f40
frenkel34 <benusfrank@gmail.com>
2014-12-03 13:03:18 +0100

    app id

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.module('sociogram', ['ionic', 'openfb', 'sociogram.controllers'])
 
     .run(function ($rootScope, $state, $ionicPlatform, $window, OpenFB) {
 
-        OpenFB.init('YOUR_FB_APP_ID');
+        OpenFB.init('881623415189803');
 
         $ionicPlatform.ready(function () {
             if (window.StatusBar) {
